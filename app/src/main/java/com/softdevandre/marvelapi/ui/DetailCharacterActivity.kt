fbd18a039a77b7f0d784c5ade1e4e7877cddf191
softdevandre <softdev.andre@gmail.com>
2022-10-01 22:00:50 -0300

    created detail character activity and layout to test navigation

---
+++ app/src/main/java/com/softdevandre/marvelapi/ui/DetailCharacterActivity.kt
@@ -0,0 +1,12 @@
+package com.softdevandre.marvelapi.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.softdevandre.marvelapi.R
+
+class DetailCharacterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_character)
+    }
+}
