bb572bf062b04746f4cdaa98f08f5225e4674a8b
softdevandre <softdev.andre@gmail.com>
2022-10-01 22:03:36 -0300

    setted up intent to navigate from MainActivity to DetailCharacterActivity

--- app/src/main/java/com/softdevandre/marvelapi/ui/adapter/ItemCharacterAdapter.kt
+++ app/src/main/java/com/softdevandre/marvelapi/ui/adapter/ItemCharacterAdapter.kt
@@ -1,21 +1,25 @@
 package com.softdevandre.marvelapi.ui.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.card.MaterialCardView
 import com.softdevandre.marvelapi.databinding.ItemCharacterBinding
 import com.softdevandre.marvelapi.model.Characters
+import com.softdevandre.marvelapi.ui.DetailCharacterActivity
 
 class ItemCharacterAdapter(private val context: Context, private val dataset: List<Characters>) :
     RecyclerView.Adapter<ItemCharacterAdapter.CharacterViewHolder>() {
 
-    class CharacterViewHolder(private val binding: ItemCharacterBinding) :
+    class CharacterViewHolder(binding: ItemCharacterBinding) :
         RecyclerView.ViewHolder(binding.root) {
         val textView: TextView = binding.tvCharacterName
         val imageView: ImageView = binding.ivCharacter
+        val materialCard: MaterialCardView = binding.mcvCharacter
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder {
@@ -29,6 +33,10 @@ class ItemCharacterAdapter(private val context: Context, private val dataset: Li
         val item = dataset[position]
         holder.textView.text = context.resources.getString(item.name)
         holder.imageView.setImageResource(item.image)
+        holder.materialCard.setOnClickListener {
+            val intent = Intent(context, DetailCharacterActivity::class.java)
+            context.startActivity(intent)
+        }
     }
 
     override fun getItemCount() = dataset.size
